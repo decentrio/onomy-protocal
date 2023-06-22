5b200f33cd778da95b31bbe9bdcaa49a1984085d
Aaron Kutch <aaronkutch@att.net>
2023-06-21 23:08:42 -0500

    remove old gravity subcommand function

--- cmd/onomyd/cmd/cmd.go
+++ cmd/onomyd/cmd/cmd.go
@@ -2,12 +2,8 @@
 package cmd
 
 import (
-	"fmt"
-
-	"github.com/cosmos/cosmos-sdk/client/flags"
 	"github.com/spf13/cobra"
 	"github.com/tendermint/starport/starport/pkg/cosmoscmd"
-	"github.com/tendermint/tendermint/libs/cli"
 
 	"github.com/onomyprotocol/onomy/app"
 )
@@ -24,25 +20,4 @@ func NewRootCmd() (*cobra.Command, cosmoscmd.EncodingConfig) {
 	)
 
 	return rootCmd, encodingConfig
-}
-
-// WrapBridgeCommands registers a sub-tree of gravity commands.
-func WrapBridgeCommands(defaultNodeHome, rootCmd string, cmds []*cobra.Command) *cobra.Command {
-	//nolint: exhaustivestruct
-	cmd := &cobra.Command{
-		Use:   rootCmd,
-		Short: fmt.Sprintf("Manage %s bridge.", rootCmd),
-		Long:  fmt.Sprintf("Manage %s bridge.", rootCmd),
-	}
-
-	for _, childCmd := range cmds {
-		cmd.AddCommand(childCmd)
-	}
-
-	cmd.PersistentFlags().String(flags.FlagHome, defaultNodeHome, "The application home directory")
-	cmd.PersistentFlags().String(flags.FlagKeyringDir, "", "The client Keyring directory; if omitted, the default 'home' directory will be used")
-	cmd.PersistentFlags().String(flags.FlagKeyringBackend, flags.DefaultKeyringBackend, "Select keyring's backend (os|file|test)")
-	cmd.PersistentFlags().String(cli.OutputFlag, "text", "Output format (text|json)")
-
-	return cmd
 }
